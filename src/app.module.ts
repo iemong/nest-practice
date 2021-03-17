05f7fffb6de6179dffb4b222930fe8d7fb45d0b0
iemong <kiyoshidainagon@gmail.com>
2021-03-17 13:03:59 +0900

    　typeOrmのentityを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5432,
+      database: 'todo',
+      host: 'localhost',
+      username: 'root',
+      password: 'root',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+    }),
+  ],
 })
 export class AppModule {}
