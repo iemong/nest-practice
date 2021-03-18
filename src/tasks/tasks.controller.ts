c94013096febd7bb6bbfd22d6dbe87a521284d46
iemong <kiyoshidainagon@gmail.com>
2021-03-18 10:45:41 +0900

    controllerにserviceの処理を組み込む

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,37 +12,40 @@ import {
 } from '@nestjs/common';
 import { TaskPropertyDto } from './dto/task-property.dto';
 import { TaskStatusPipe } from './pipe/task-status.pipe';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.entity';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get()
-  getTasks() {
-    return 'get tasks success!';
+  getTasks(): Promise<Task[]> {
+    return this.tasksService.getTasks();
   }
 
   @Get('/:id')
-  getTaskById(@Param('id', ParseIntPipe) id: number) {
-    return `getTaskById Success! Parameter [id:${id}]`;
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
   }
 
   // NOTE: ParseIntPipeはパイプという機能の1つで、コントローラのメソッドに値が引き渡される前に変換、検証を行う。@Param('id', ParseIntPipe)ではidを数値型へと変換します。(変換できなかった場合は例外をスローします)
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() taskPropertyDto: TaskPropertyDto) {
-    const { title, description } = taskPropertyDto;
-    return `createTask Success! Prameter [title:${title}, descritpion:${description}]`;
+  createTask(@Body() taskPropertyDto: TaskPropertyDto): Promise<Task> {
+    return this.tasksService.createTask(taskPropertyDto);
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number) {
-    return `deleteTask Success! Prameter [id:${id}]`;
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
   }
 
   @Patch('/:id')
   updateTask(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusPipe) status: string,
-  ) {
-    return `updateTask Success! Prameter [id:${id}, status:${status}]`;
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, status);
   }
 }
